b4364c2ed652a50b860b1d51f99fc53c40d4da41
Rjrohan <rjrohan0340@gmail.com>
2026-04-21 23:50:45 +0530

    cors flight handling

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -27,8 +27,7 @@ fi
 # Nginx config
 cat > /etc/nginx/sites-available/cloudproof <<EOF
 server {
-    listen 80;
-    server_name _;
+    server_name handsoncloud.in;
 
     location / {
         root /var/www/html;
@@ -40,7 +39,31 @@ server {
         proxy_pass http://127.0.0.1:5000;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+
+        if (\$request_method = OPTIONS) {
+            add_header Access-Control-Allow-Origin \$http_origin;
+            add_header Access-Control-Allow-Methods 'GET, POST, PUT, DELETE, OPTIONS';
+            add_header Access-Control-Allow-Headers 'Content-Type, Authorization';
+            add_header Access-Control-Allow-Credentials 'true';
+            return 204;
+        }
     }
+
+    listen 443 ssl;
+    ssl_certificate /etc/letsencrypt/live/handsoncloud.in/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/handsoncloud.in/privkey.pem;
+    include /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+}
+
+server {
+    if (\$host = handsoncloud.in) {
+        return 301 https://\$host\$request_uri;
+    }
+    listen 80;
+    server_name handsoncloud.in;
+    return 404;
 }
 EOF
 
